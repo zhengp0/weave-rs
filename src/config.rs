0037fe9f23ef73b3e23932eab5deacaf3f5583f1
Peng Zheng <zhengp@uw.edu>
2023-10-29 20:11:59 +0800

    add configuration module

---
+++ src/config.rs
@@ -0,0 +1,45 @@
+use serde::Deserialize;
+use std::{error, fs, path};
+use toml;
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub datasets: DatasetsConfig,
+    pub dimensions: Vec<DimensionConfig>,
+}
+
+impl Config {
+    pub fn from_file<P: AsRef<path::Path>>(path: P) -> Result<Config, Box<dyn error::Error>> {
+        let file = fs::read_to_string(path)?;
+        let config = toml::from_str(&file)?;
+        Ok(config)
+    }
+}
+
+#[derive(Deserialize)]
+pub struct DatasetsConfig {
+    pub data: path::PathBuf,
+    pub pred: path::PathBuf,
+}
+
+#[derive(Deserialize)]
+pub struct DimensionConfig {
+    pub key: Vec<String>,
+    pub distance: DistanceConfig,
+    pub kernel: KernelConfig,
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "type")]
+pub enum DistanceConfig {
+    Euclidean,
+    Tree,
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "type")]
+pub enum KernelConfig {
+    Exponential { radius: f32 },
+    Tricubic { radius: f32, exponent: f32 },
+    DepthCODEm { radius: f32, maxlvl: i32 },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use weavers::config::Config;
+
 fn main() {
-    println!("Thanks for using weavers!");
+    let args: Vec<String> = std::env::args().collect();
+    let config = Config::from_file(&args[1]).expect("have trouble loading the file");
+
+    println!(
+        "datasets: data: {}, pred: {}",
+        config.datasets.data.display(),
+        config.datasets.pred.display(),
+    );
 }
