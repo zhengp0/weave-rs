63df897b63573a7ee1b9fee9f25cfc2f7162f23f
Peng Zheng <zhengp@uw.edu>
2023-11-07 18:34:10 +0800

    convert to use static dispatch with enum

--- src/model/dimenion.rs
+++ src/model/dimenion.rs
@@ -1,109 +1,120 @@
 use crate::model::{distance::Distance, kernel::Kernel};
-use std::{collections::HashMap, hash::Hash};
+use std::collections::HashMap;
 
-pub struct Coords<T> {
+pub struct CoordsData<T> {
     pub data: Vec<Vec<T>>,
     pub pred: Vec<Vec<T>>,
 }
 
-pub struct DimensionInfo<D, K>
-where
-    D: Distance,
-    K: Kernel<Input = D::Output>,
-{
-    pub distance: D,
-    pub kernel: K,
-    pub coords: Coords<D::Input>,
+pub enum Coords {
+    I32(CoordsData<i32>),
+    F32(CoordsData<f32>),
 }
 
-impl<D, K> DimensionInfo<D, K>
-where
-    D: Distance,
-    K: Kernel<Input = D::Output>,
-{
-    pub fn new(distance: D, kernel: K, coords: Coords<D::Input>) -> Self {
+pub enum Processor {
+    Generic,
+    Categorical,
+}
+
+pub struct Dimension {
+    pub distance: Distance,
+    pub kernel: Kernel,
+    pub coords: Coords,
+    pub processor: Processor,
+}
+
+impl Dimension {
+    pub fn new(distance: Distance, kernel: Kernel, coords: Coords, processor: Processor) -> Self {
         Self {
             distance,
             kernel,
             coords,
+            processor,
         }
     }
-}
 
-pub struct Generic<D, K>(pub DimensionInfo<D, K>)
-where
-    D: Distance,
-    K: Kernel<Input = D::Output>;
+    pub fn update_weight(&self, i: usize, weight: &mut Vec<f32>) {
+        match self {
+            Self {
+                distance: Distance::Euclidean(distance_fn),
+                kernel: Kernel::Exponential(kernel_fn),
+                coords: Coords::F32(coords),
+                processor: Processor::Generic,
+            } => {
+                let x = &coords.data[i];
+                let y_iter = coords.pred.iter();
+                for (y, w) in y_iter.zip(weight.iter_mut()) {
+                    *w *= kernel_fn.call(&distance_fn.call(x, y));
+                }
+            }
+            Self {
+                distance: Distance::Euclidean(distance_fn),
+                kernel: Kernel::Tricubic(kernel_fn),
+                coords: Coords::F32(coords),
+                processor: Processor::Generic,
+            } => {
+                let x = &coords.data[i];
+                let y_iter = coords.pred.iter();
+                for (y, w) in y_iter.zip(weight.iter_mut()) {
+                    *w *= kernel_fn.call(&distance_fn.call(x, y));
+                }
+            }
+            Self {
+                distance: Distance::Tree(distance_fn),
+                kernel: Kernel::DepthCODEm(kernel_fn),
+                coords: Coords::I32(coords),
+                processor: Processor::Generic,
+            } => {
+                let x = &coords.data[i];
+                let y_iter = coords.pred.iter();
+                for (y, w) in y_iter.zip(weight.iter_mut()) {
+                    *w *= kernel_fn.call(&distance_fn.call(x, y));
+                }
+            }
+            Self {
+                distance: Distance::Tree(distance_fn),
+                kernel: Kernel::DepthCODEm(kernel_fn),
+                coords: Coords::I32(coords),
+                processor: Processor::Categorical,
+            } => {
+                let x = &coords.data[i];
+                let y_iter = coords.pred.iter();
+                let mut norm_map = HashMap::new();
+                let distance: Vec<i32> = y_iter.map(|y| distance_fn.call(x, y)).collect();
 
-pub struct Categorical<D, K>(pub DimensionInfo<D, K>)
-where
-    D: Distance,
-    K: Kernel<Input = D::Output>,
-    D::Output: Eq + Hash + Copy;
-
-pub trait Dimension {
-    fn update_weight(&self, i: usize, weight: &mut Vec<f32>);
-}
-
-impl<D, K> Dimension for Generic<D, K>
-where
-    D: Distance,
-    K: Kernel<Input = D::Output>,
-{
-    fn update_weight(&self, i: usize, weight: &mut Vec<f32>) {
-        let info = &self.0;
-        let x = &info.coords.data[i];
-        let y_iter = info.coords.pred.iter();
-        for (y, w) in y_iter.zip(weight.iter_mut()) {
-            *w *= self.0.kernel.call(&info.distance.call(x, y));
-        }
-    }
-}
-
-impl<D, K> Dimension for Categorical<D, K>
-where
-    D: Distance,
-    K: Kernel<Input = D::Output>,
-    D::Output: Eq + Hash + Copy,
-{
-    fn update_weight(&self, i: usize, weight: &mut Vec<f32>) {
-        let info = &self.0;
-        let x = &info.coords.data[i];
-        let y_iter = info.coords.pred.iter();
-        let mut norm_map = HashMap::new();
-        let distance: Vec<D::Output> = y_iter.map(|y| info.distance.call(x, y)).collect();
-
-        for (d, w) in distance.iter().zip(weight.iter()) {
-            norm_map.entry(d).and_modify(|x| *x += w).or_insert(*w);
-        }
-        for (d, w) in distance.iter().zip(weight.iter_mut()) {
-            *w *= info.kernel.call(d) / &norm_map[d];
+                for (d, w) in distance.iter().zip(weight.iter()) {
+                    norm_map.entry(d).and_modify(|x| *x += w).or_insert(*w);
+                }
+                for (d, w) in distance.iter().zip(weight.iter_mut()) {
+                    *w *= kernel_fn.call(d) / &norm_map[d];
+                }
+            }
+            _ => panic!("cannot update weight"),
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::model::{distance::Tree, kernel::DepthCODEm};
+    use crate::model::{distance::TreeFn, kernel::DepthCODEmFn};
 
-    fn info() -> DimensionInfo<Tree, DepthCODEm> {
-        let distance = Tree;
-        let kernel = DepthCODEm::new(0.5, 3);
-        let coords = Coords {
+    use super::*;
+
+    fn coords() -> Coords {
+        Coords::I32(CoordsData {
             data: vec![vec![0, 1, 2]],
             pred: vec![vec![0, 1, 2], vec![0, 1, 8], vec![0, 6, 7], vec![3, 4, 5]],
-        };
-        DimensionInfo {
-            distance,
-            kernel,
-            coords,
-        }
+        })
     }
 
     #[test]
     fn test_generic_update_weight() {
-        let dimension = Generic(info());
+        let dimension = Dimension::new(
+            Distance::Tree(TreeFn),
+            Kernel::DepthCODEm(DepthCODEmFn::new(0.5, 3)),
+            coords(),
+            Processor::Generic,
+        );
         let mut my_weight: Vec<f32> = vec![1.0; 4];
         dimension.update_weight(0, &mut my_weight);
         let ok_weight = vec![0.5, 0.25, 0.25, 0.0];
@@ -112,7 +123,12 @@ mod tests {
 
     #[test]
     fn test_categorical_update_weight() {
-        let dimension = Categorical(info());
+        let dimension = Dimension::new(
+            Distance::Tree(TreeFn),
+            Kernel::DepthCODEm(DepthCODEmFn::new(0.5, 3)),
+            coords(),
+            Processor::Categorical,
+        );
         let mut my_weight: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0];
         dimension.update_weight(0, &mut my_weight);
         let ok_weight = vec![0.5, 0.25, 0.25, 0.0];
--- src/model/distance.rs
+++ src/model/distance.rs
@@ -1,31 +1,21 @@
-pub trait Distance {
-    type Input;
-    type Output;
-
-    fn call(&self, x: &Vec<Self::Input>, y: &Vec<Self::Input>) -> Self::Output;
+pub enum Distance {
+    Euclidean(EuclideanFn),
+    Tree(TreeFn),
 }
 
-pub struct Euclidean;
-
-impl Distance for Euclidean {
-    type Input = f32;
-    type Output = f32;
-
-    fn call(&self, x: &Vec<Self::Input>, y: &Vec<Self::Input>) -> Self::Output {
+pub struct EuclideanFn;
+impl EuclideanFn {
+    pub fn call(&self, x: &Vec<f32>, y: &Vec<f32>) -> f32 {
         (x[0] - y[0]).abs()
     }
 }
 
-pub struct Tree;
-
-impl Distance for Tree {
-    type Input = i32;
-    type Output = i32;
-
-    fn call(&self, x: &Vec<Self::Input>, y: &Vec<Self::Input>) -> Self::Output {
+pub struct TreeFn;
+impl TreeFn {
+    pub fn call(&self, x: &Vec<i32>, y: &Vec<i32>) -> i32 {
         let x_iter = x.iter().rev();
         let y_iter = y.iter().rev();
-        x_iter.zip(y_iter).take_while(|(xi, yi)| xi != yi).count() as Self::Output
+        x_iter.zip(y_iter).take_while(|(xi, yi)| xi != yi).count() as i32
     }
 }
 
@@ -35,7 +25,7 @@ mod tests {
 
     #[test]
     fn test_euclidean() {
-        let distance_fn = Euclidean;
+        let distance_fn = EuclideanFn;
         let x: Vec<f32> = vec![0.0];
         let y: Vec<f32> = vec![1.0];
 
@@ -46,7 +36,7 @@ mod tests {
 
     #[test]
     fn test_tree() {
-        let distance_fn = Tree;
+        let distance_fn = TreeFn;
 
         let x = vec![0, 1, 2];
         let y_vec = vec![vec![3, 4, 5], vec![0, 6, 7], vec![0, 1, 8], vec![0, 1, 2]];
